069f77e0661b1e757c14f5f058c2903b08f6336a
AnnaBeatrizSuritaManuel <anna.manuel@etec.sp.gov.br>
2025-12-01 14:18:39 -0300

    olja que leagal

--- App.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-function App() {
-  const value = 'Bebe';
-  return <div>Hello {value}</div>;
-}
-
-export default App;
--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function App() {
   const value = 'Bebe';
-  return <div>Hello {value}</div>;
+  return <div> {value}</div>;
 }
 
 export default App;
